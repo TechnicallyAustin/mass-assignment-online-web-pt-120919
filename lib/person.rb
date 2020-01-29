8cf3da1bd68288c52394dde26ea9b8c952aa8b53
Austin Johnson <technicallyaustin@gmail.com>
2020-01-29 22:35:03 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,5 +3,5 @@ class Person
  
  attributes = name:, brthday:, hair_color:, eye_color:, height:, weight:, handed:, complexion:, t_shirt_size:, wrist_size:, glove_size:, pant_length:, pant_width:
  def initialize(attributes)
-   attributes
+   attributes.each { |key,value| self.send(("#{key}="), value)
 end
