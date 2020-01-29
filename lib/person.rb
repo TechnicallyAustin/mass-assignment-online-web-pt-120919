ca22f84a9c1ba4b20319e5c4568b0037d873ee37
Austin Johnson <technicallyaustin@gmail.com>
2020-01-29 22:33:21 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,7 @@
 class Person
- attr_accessor :name, :brthday
+ attr_accessor :name, :brthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+ 
+ attributes = name:, brthday:, hair_color:, eye_color:, height:, weight:, handed:, complexion:, t_shirt_size:, wrist_size:, glove_size:, pant_length:, pant_width:
+ def initialize(attributes)
+   attributes
 end
